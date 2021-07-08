4d7290f12de80e5d5db8e1a75d171bb9c61bbc53
Gleb Krasnov <glebon0202@gmail.com>
2021-07-08 15:01:33 +0200

    Reviews modified

--- bin/reviews/main.dart
+++ bin/reviews/main.dart
@@ -7,88 +7,111 @@ import 'models/review.dart';
 import '../companies/encode/encode_classes.dart';
 
 void main() async {
-  await _create();
+  await _create(country: 'UA');
 }
 
-Future<void> _create() async {
-  final File file = File('bin/reviews/source/UA.xlsx');
-  final bytes = file.readAsBytesSync();
-  final Excel excel = Excel.decodeBytes(bytes);
-  final String table = excel.tables.keys.first;
+Future<void> _create({
+  String country,
+}) async {
+  final File fileExcel = File('bin/reviews/source/UA.xlsx');
+  final Excel excel = Excel.decodeBytes(
+    fileExcel.readAsBytesSync(),
+  );
+  final String tableExcel = excel.tables.keys.first;
+
+  final Map companiesFile = jsonDecode(await File(
+    'bin/reviews/source/import-$country.json',
+  ).readAsString())['__collections__']['countries']['$country']
+      ['__collections__']['companies'];
 
   final Map outputCompanyMap = {};
 
   final Map outputReviewsMap = {};
 
-  // Start from second row
-  for (int rowIn = 1; rowIn < excel.tables[table].rows.length; rowIn++) {
-    // Set current row
-    final List<Data> row = excel.tables[table].rows[rowIn];
-    final Map reviewsMap = {};
+  // Начинаем обход со второй строки
+  for (int rowIn = 1; rowIn < excel.tables[tableExcel].rows.length; rowIn++) {
+    // Устанавливаем текущую строку
+    final List<Data> row = excel.tables[tableExcel].rows[rowIn];
 
-    // 1) Создаём объект компанию (если существует в json) получая данные
-    // из неё иначе просто переходим к следующему объекту
-    final Data company = row[1];
-    print('*** COMPANY: $company ***');
+    final Map companyFile = companiesFile.entries.firstWhere((e) {
+      return e.value['siteURL'] == row[1].value.trim();
+    }, orElse: () => null)?.value;
 
-    final String compUid = '7e6300c4-13c6-4012-9df7-a22839a6e6c1';
+    if (companyFile != null) {
+      int numOfReviews = 0;
 
-    // 2) Создаём объект Review
-    for (int cellIn = 4; cellIn < row.length;) {
-      final String displayName = row[cellIn]?.value;
-      cellIn++;
-      final double rating = row[cellIn]?.value?.toDouble();
-      cellIn++;
-      final String message = row[cellIn]?.value;
-      cellIn++;
+      double totalRating = 0;
 
-      // Если по каким-то параметрам он не подходит не добавляем его в
-      // outputReviewsMap либо заменяем какие-то данные и добавляем в outputReviewsMap:
-      //
-      // 1. Отзывы с оценкой 0, но с текстом и автором - необходимо импортировать с оценкой 4
-      // 2. Отзывы без автора, но с текстом и оценкой - необходимо импортировать с указанием автора Anonymous
-      // 3. Отзывы без текста, но с автором и оценкой - не импортируем
+      final Map reviewsMap = {};
 
-      if (message != null) {
-        final String uid = Uid.getUid();
+      // Создаём объект Review
+      for (int cellIn = 4; cellIn < row.length;) {
+        final String displayName = row[cellIn]?.value;
+        cellIn++;
+        final double rating = row[cellIn]?.value?.toDouble();
+        cellIn++;
+        final String message = row[cellIn]?.value;
+        cellIn++;
 
-        reviewsMap[uid] = Review(
-          uid: uid,
-          uidOwner: Uid.getUid(),
-          displayName: displayName,
-          message: message,
-          rating: rating == 0.0 ? 4.0 : rating,
-        ).toMap();
-      }
-    }
+        // 1. Отзывы с оценкой 0, но с текстом и автором - необходимо импортировать с оценкой 4
+        // 2. Отзывы без автора, но с текстом и оценкой - необходимо импортировать с указанием автора Anonymous
+        // 3. Отзывы без текста, но с автором и оценкой - не импортируем
 
-    if (reviewsMap.isNotEmpty) {
-      outputReviewsMap[compUid] = {
-        '__collections__': {
-          'clients': reviewsMap,
+        if (message != null) {
+          final String uid = Uid.getUid();
+
+          reviewsMap[uid] = Review(
+            uid: uid,
+            uidOwner: Uid.getUid(),
+            displayName: displayName,
+            message: message,
+            rating: rating == 0.0 ? 4.0 : rating,
+          ).toMap();
+
+          numOfReviews++;
+          totalRating = totalRating + rating;
         }
-      };
+      }
+
+      if (reviewsMap.isNotEmpty) {
+        companyFile['displayName'] = companyFile['displayName'].trim();
+        companyFile['rating']['totalRating'] = totalRating;
+        companyFile['rating']['numOfReviews'] = numOfReviews;
+
+        outputCompanyMap.putIfAbsent(
+          companyFile['uid'],
+          () => companyFile,
+        );
+
+        outputReviewsMap.putIfAbsent(
+          companyFile['uid'],
+          () => {
+            '__collections__': {
+              'clients': reviewsMap,
+            }
+          },
+        );
+      }
     }
   }
 
-  // Преобразуем outputReviewsMap и outputReviewsMap в вид в котором будем
-  // экспортировать в файл json для отправки в Firebase
-
   // Write to file
-  final File importFile = File('bin/reviews/output/reviews-test.json');
+  final File importFile = File(
+    'bin/reviews/output/import-reviews-$country.json',
+  );
+
   importFile.writeAsStringSync(
     jsonEncode({
       '__collections__': {
-        'reviews': outputReviewsMap,
-      }
-    }),
-  );
-
-  /*'countries': {
-          'UA': {
+        'countries': {
+          '$country': {
             '__collections__': {
               'companies': outputCompanyMap,
             }
           }
-        },*/
+        },
+        'reviews': outputReviewsMap,
+      }
+    }),
+  );
 }
