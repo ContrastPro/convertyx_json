8e0b7a609240db5ded194540b085eac51fe681bc
Gleb Krasnov <glebon0202@gmail.com>
2021-07-08 17:55:29 +0200

    Reviews modified

--- bin/reviews/test.dart
+++ bin/reviews/test.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:excel/excel.dart';
@@ -14,9 +15,18 @@ int _numOfReviews = 0;
 double _totalRating = 0.0;
 Map _reviewsMap = {};
 
+Map _outputCompanies = {};
+Map _outputReviews = {};
+
 Future<void> _convertReviews({
   String country,
 }) async {
+  final Map companiesInMap = jsonDecode(
+    await File(
+      'bin/reviews/source/import-$country.json',
+    ).readAsString(),
+  )['__collections__']['countries']['$country']['__collections__']['companies'];
+
   final Excel excel = Excel.decodeBytes(
     File('bin/reviews/source/UA.xlsx').readAsBytesSync(),
   );
@@ -38,7 +48,7 @@ Future<void> _convertReviews({
       if (message != null) {
         final String uid = Uid.getUid();
 
-        _reviewsMap['UID-$uid'] = {
+        _reviewsMap[uid] = {
           'displayName': displayName,
           'rating': rating,
           'message': message,
@@ -50,32 +60,66 @@ Future<void> _convertReviews({
     }
 
     if (_reviewsMap.isNotEmpty) {
+      final Map companyInMap = companiesInMap.entries.firstWhere((e) {
+        return e.value['siteURL'] == row[1].value.trim();
+      }, orElse: () => null)?.value;
+
       if (i + 1 < excel.tables[tableExcel].rows.length) {
         final List<Data> nextRow = excel.tables[tableExcel].rows[i + 1];
 
         if (nextRow[1].value != _siteURL) {
-          print(_reviewsMap);
-          print(
-            '\nTOTAL: <$_siteURL> '
-            'REVIEWS: $_numOfReviews '
-            'RATING: $_totalRating\n',
-          );
+          await _addToOutputMap(compUid: companyInMap['uid']);
           _numOfReviews = 0;
           _totalRating = 0.0;
           _reviewsMap.clear();
         }
       } else {
-        print(_reviewsMap);
-        print(
-          '\nLAST TOTAL: <$_siteURL> '
-          'REVIEWS: $_numOfReviews '
-          'RATING: $_totalRating\n',
-        );
+        await _addToOutputFile();
       }
     }
   }
 }
 
-Future<void> _save() async {
-  //
+Future<void> _addToOutputMap({String compUid}) async {
+  _outputReviews[compUid] = {
+    '__collections__': {
+      'clients': _reviewsMap,
+    }
+  };
+
+  print(_reviewsMap);
+  print(
+    '\nTOTAL: <$_siteURL> $compUid '
+    'REVIEWS: $_numOfReviews '
+    'RATING: $_totalRating\n',
+  );
+}
+
+Future<void> _addToOutputFile() async {
+  // Write to file
+  final File importFile = File(
+    'bin/reviews/output/import-reviews-UA.json',
+  );
+
+  importFile.writeAsStringSync(
+    jsonEncode({
+      '__collections__': {
+        /*'countries': {
+          '$country': {
+            '__collections__': {
+              'companies': outputCompanyMap,
+            }
+          }
+        },*/
+        'reviews': _outputReviews,
+      }
+    }),
+  );
+
+  print(_reviewsMap);
+  print(
+    '\nLAST TOTAL: <$_siteURL> '
+    'REVIEWS: $_numOfReviews '
+    'RATING: $_totalRating\n',
+  );
 }
